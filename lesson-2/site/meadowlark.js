cb5b9cb43ece048f12f03006e861403101f18786
sunguozhi <sunguozhi@syswin.com>
2016-04-07 14:41:08 +0800

    Moved 'fortune cookie' functionality into module.

--- lesson-2/site/meadowlark.js
+++ lesson-2/site/meadowlark.js
@@ -17,10 +17,10 @@ app.get('/', function(req, res){
 	res.render('home');
 });
 
+var fortune = require('./lib/fortune.js');
 app.get('/about', function(req, res){
-	var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
 	res.render('about', {
-		fortune: randomFortune
+		fortune: fortune.getFortune()
 	});
 });
 
@@ -42,10 +42,3 @@ app.listen(app.get('port'),function(){
 	console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate');
 });
 
-var fortunes = [
-	"Conquer your fear or they will conquer you.",
-	"Rivers need springs.",
-	"Do not fear what you don't know.",
-	"You will have a pleasant surprice.",
-	"Whenever possible, keep it simple",
-];
