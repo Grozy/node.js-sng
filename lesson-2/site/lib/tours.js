04698c37bb94157f5071472ec1919e620571835b
sunguozhi <sunguozhi@syswin.com>
2016-04-08 18:41:36 +0800

    express 布局的使用

---
+++ lesson-2/site/lib/tours.js
@@ -0,0 +1,17 @@
+exports.getAllTours = function(){
+	return tours;
+};
+
+var tours = [
+	{
+		id: 0,
+		name: 'Hood River',
+		price: 99.99
+	},
+	{
+		id: 1,
+		name: 'Oregon Coast',
+		price: 149.95
+
+	}
+];
--- lesson-2/site/meadowlark.js
+++ lesson-2/site/meadowlark.js
@@ -12,6 +12,13 @@ var handlebars = require('express3-handlebars').create( {
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+// 创建一个中间件
+app.use(function(req, res, next){
+	if(!res.locals.partials) res.locals.partials = {};
+	res.locals.partials.weather = getWeatherData();
+	next();
+});
+
 //新增路由
 app.get('/', function(req, res){
 	res.render('home');
@@ -20,10 +27,33 @@ app.get('/', function(req, res){
 var fortune = require('./lib/fortune.js');
 app.get('/about', function(req, res){
 	res.render('about', {
-		fortune: fortune.getFortune()
+		fortune: fortune.getFortune(),
+		pageTestScript: '/qa/test-about.js'
 	});
 });
 
+// tours
+app.get('/tours/hood-river', function(req, res){
+	res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', function(req, res){
+	res.render('tours/request-group-rate');
+});
+
+// read header
+app.get('/headers', function(req, res){
+	res.set('Content-Type', 'text/plain');
+	var s = '';
+	for(var name in req.headers) s += name + ': ' +req.headers[name] + '\n';
+	res.send(s);
+});
+
+// api 
+var tours = require('./lib/tours.js')
+app.get('/api/tours', function(req, res){
+	res.json(tours.getAllTours());
+});
 
 //定制404页面
 app.use(function(req, res){
@@ -36,9 +66,37 @@ app.use(function(err, req, res, next){
 	console.error(err.stack);
 	res.status(500);
 	res.render('500');
+	next();
 });
 
 app.listen(app.get('port'),function(){
 	console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate');
 });
 
+function getWeatherData(){
+	return {
+		locations:[
+			{
+				name: 'Portland',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+				weather: 'Overcast',
+	            temp: '54.1 F (12.3 C)',
+			},
+			{
+				name: 'Bend',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+				weather: 'Partly Cloudy',
+				temp: '55.0 F (12.8 C)',
+			},
+			{
+				name: 'Manzanita',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+				weather: 'Light Rain',
+				temp: '55.0 F (12.8 C)',
+			}
+		]
+	};
+}
