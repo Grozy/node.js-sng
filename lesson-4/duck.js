03984349ab0f654979882a088fa38724ea2caeef
sunguozhi <sunguozhi@syswin.com>
2016-05-06 09:57:40 +0800

    多态

--- lesson-4/duck.js
+++ lesson-4/duck.js
@@ -32,7 +32,8 @@ joinChoir(duck);
 joinChoir(chicken);
 joinChoir(doggy);
 */
-
+/*
+// code 2
 var makeSound = function(animal) {
   if (animal instanceof Duck) {
     console.log('嘎嘎嘎');
@@ -46,3 +47,79 @@ var Chicken = function(){};
 
 makeSound(new Duck());
 makeSound(new Chicken());
+*/
+/*
+// code 1.2.3
+var makeSound = function(animal){
+  animal.sound();
+};
+
+var Duck = function(){};
+
+Duck.prototype.sound = function(){
+  console.log('嘎嘎嘎');
+};
+
+var Chicken = function(){};
+
+Chicken.prototype.sound = function(){
+  console.log('咯咯咯');
+};
+
+var Doggy = function(){};
+
+Doggy.prototype.sound = function () {
+  console.log('汪汪汪');
+};
+
+makeSound(new Duck());
+makeSound(new Chicken());
+makeSound(new Doggy());
+*/
+
+var googleMap = {
+  show: function(){
+    console.log('开始渲染谷歌地图');
+  }
+}
+
+/*
+var renderMap = function(){
+  googleMap.show();
+}
+*/
+
+// 将谷歌地图改成百度地图
+var baiduMap = {
+  show: function(){
+    console.log('开始渲染百度地图');
+  }
+}
+/*
+// 根据type选择调用方案
+var renderMap = function(type){
+  if (type === 'google') {
+    googleMap.show();
+  } else if ( type === 'baidu') {
+    baiduMap.show();
+  }
+}
+renderMap('baidu');
+// renderMap('google');
+*/
+
+var renderMap = function(map) {
+  if (map.show instanceof Function) {
+    map.show()
+  }
+}
+
+renderMap(baiduMap);
+// renderMap(googleMap);
+// 新增soso地图
+// var sosoMap = {
+//   show: function(){
+//     console.log('开始渲染搜搜地图');
+//   }
+// }
+// renderMap(sosoMap);
