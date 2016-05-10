d59adf5e13e2aa63a9d00478708eb029de78900e
sunguozhi <sunguozhi@syswin.com>
2016-05-10 13:16:23 +0800

    添加处理

--- lesson-2/sites/blog/routes/wechat.js
+++ lesson-2/sites/blog/routes/wechat.js
@@ -5,6 +5,11 @@ var router = express.Router();
 router.use('/', wechat('sng_sc_1991', function(req, res, next) {
   var message = req.weixin;
   console.log(message);
+  if (message.MsgType == 'text') {
+    res.reply('别发字符串！！');
+  } else if (message.MsgType == 'image') {
+    res.reply('图片也是一样的！！');
+  }
 }));
 
 router.get('/', function(req, res, next) {
