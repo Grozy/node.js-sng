954aa6428ace94f73658cbcb44fc007d0c5ce054
sunguozhi <sunguozhi@syswin.com>
2016-05-10 13:08:52 +0800

    modify wechat router

--- lesson-2/sites/blog/routes/wechat.js
+++ lesson-2/sites/blog/routes/wechat.js
@@ -1,8 +1,14 @@
 var express = require('express');
+var wechat = require('wechat');
 var router = express.Router();
 /* GET users listing. */
+router.use('/', wechat('sng_sc_1991', function(req, res, next) {
+
+}));
+
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+
 });
 
 module.exports = router;
