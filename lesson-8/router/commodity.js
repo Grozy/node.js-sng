6bf1a6a0f4b26f6d723c0934b0d3e56f616bd33b
guozhi <grozysun@gmail.com>
2016-11-27 13:00:22 +0800

    modify info

--- lesson-8/router/commodity.js
+++ lesson-8/router/commodity.js
@@ -5,11 +5,9 @@ const Commodity = require('../model/commodity');
 router.get('/', function(req, res){
   console.log('enter commodity index...');
   Commodity.queryWithLJM(req.query.ljm, function(err, commoditys) {
-    console.log("err" + commoditys);
+    console.log("err:" + commoditys);
     res.send(commoditys);//登出成功后跳转到主页
   });
 });
-
-router.get('/info', function)
 
 module.exports = router;
